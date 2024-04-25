88d26bce82a54bd8c6699f1c5418dcfab4558938
Timo K <toger5@hotmail.de>
2024-04-25 22:07:49 +0200

    temp
    Signed-off-by: Timo K <toger5@hotmail.de>

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,13 @@
 import "./style.css";
-import {
-  WidgetEventCapability,
-  EventDirection,
-  IOpenIDCredentials,
-  MatrixCapabilities,
-} from "matrix-widget-api";
-import { WidgetApiImpl } from "@matrix-widget-toolkit/api";
-import { initClient } from "./matrix-utils";
+import { IOpenIDCredentials } from "matrix-widget-api";
 import { widget } from "./widget";
-import { MatrixClient } from "matrix-js-sdk/src/matrix";
-
 const STATE_EVENT_CALL_MEMBERS = "m.call.member";
 const STATE_EVENT_ROOM_NAME = "m.room.name";
 // TODO load this from the right place
 // this should either be done by reading the current rtc session in the room or by using a config.json fallback
 const BBB_SERVICE_URL = "https://droplet-7099.meetbbb.com/service";
 
-const initialCapabilities = [
-  WidgetEventCapability.forStateEvent(
-    EventDirection.Receive,
-    STATE_EVENT_CALL_MEMBERS
-  ),
-  WidgetEventCapability.forStateEvent(
-    EventDirection.Send,
-    STATE_EVENT_CALL_MEMBERS
-  ),
-  WidgetEventCapability.forRoomEvent(EventDirection.Send, "m.room.message"),
-  WidgetEventCapability.forStateEvent(
-    EventDirection.Receive,
-    STATE_EVENT_ROOM_NAME
-  ),
-  MatrixCapabilities.AlwaysOnScreen,
-];
 async function setup() {
-  // const widgetApi = await WidgetApiImpl.create({
-  //   capabilities: initialCapabilities,
-  // });
   if (!widget) {
     console.error("Widget not found");
     throw new Error("Widget not found");
@@ -51,13 +23,8 @@ async function setup() {
     console.error("Room not found");
     throw new Error("Room not found");
   }
+
   const roomName = room.name;
-  // const roomName =
-  //   (
-  //     await widgetApi.receiveStateEvents<{ name: string }>(
-  //       STATE_EVENT_ROOM_NAME
-  //     )
-  //   )[0]?.content?.name ?? roomId;
 
   const token = await client.getOpenIdToken();
   const appContainer = document.getElementById("app");
@@ -67,6 +34,7 @@ async function setup() {
     const n = roomName;
     const r = roomId;
     appContainer.innerText = `Token: ${t}\n\nServer: ${d}\nroomName: ${n}\nroomId: ${r}\nJoinURL: unknown`;
+
     const { url } = await getBBBJoinUrl(
       deviceId,
       roomId,
@@ -76,15 +44,8 @@ async function setup() {
       token
     );
 
-    // if there is a running session join it
-
-    // if there is no running session create one with default livekit sfu.
-
     console.log("Join URL: ", url);
     appContainer.innerText = `Token: ${t}\n\nServer: ${d}\nroomName: ${n}\nroomId: ${r}\nJoinURL: ${url}`;
-    // (widgetApi as WidgetApiImpl).matrixWidgetApi.setAlwaysOnScreen(true);
-
-    // window.location.replace(url);
     const iframe = document.getElementById("widgetFrame") as HTMLIFrameElement;
     const iframeFeatures =
       "microphone *; camera *; encrypted-media *; autoplay *; display-capture *; clipboard-write *; " +
@@ -93,23 +54,36 @@ async function setup() {
     iframe.src = url;
 
     const session = client.matrixRTC.getRoomSession(room);
-    session.joinRoomSession([]);
-    widget.api.setAlwaysOnScreen(true);
-    let seconds = 0;
-    const count = (): void => {
-      if (seconds < 20) {
-        seconds++;
-        // eslint-disable-next-line no-console
-        console.log("Leaving meeting in: ", seconds);
-        appContainer.innerText = `Leave in 20 - ${seconds} Token: ${t}\n\nServer: ${d}\nroomName: ${n}\nroomId: ${r}\nJoinURL: unknown`;
+    window.onmessage = (event) => {
+      if (event.data.api !== "fromBBB") return;
 
-        setTimeout(() => count(), 1000);
-      } else {
-        session.leaveRoomSession();
-        widget?.api.setAlwaysOnScreen(false);
+      switch (event.data.action) {
+        case "leave":
+          session.leaveRoomSession();
+          widget?.api.setAlwaysOnScreen(false);
+          break;
+
+        case "join":
+          widget?.api.setAlwaysOnScreen(true);
+          session.joinRoomSession([]);
+          session.joinRoomSession([]);
+          break;
+        case "request_credentials":
+          const response = {
+            api: "toBBB",
+            action: "lk-credentials",
+            data: {
+              jwt: "test",
+              websocket_url: "test",
+              lk_alias: "test",
+            },
+          };
+          iframe.contentWindow?.postMessage(response, "*");
       }
     };
-    count();
+    // if there is a running session join it
+    session;
+    // if there is no running session create one with default livekit sfu.
   }
 }
 
